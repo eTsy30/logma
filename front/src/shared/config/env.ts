ae4a2ba38da6f81478fa776a2a650ff7953b625d
Evgen Tsygankov <tsy@mail.com>
2026-02-20 18:45:12 +0300

    fix 5

--- front/src/shared/config/env.ts
+++ front/src/shared/config/env.ts
@@ -1,3 +1,3 @@
-export const API_URL =
-  process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost:3001';
+export const API_URL = 'https://logma.onrender.com';
+//?? 'http://localhost:3001';
 console.log('API_URL:', API_URL);
